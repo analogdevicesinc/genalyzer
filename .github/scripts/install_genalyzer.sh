2deac890471c24cbcf1978d5f38bc0a1f6077e66
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2021-08-25 15:12:34 -0400

    Reordered the installation of sphinx and breathe in install_genalyzer.sh
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- .github/scripts/install_genalyzer.sh
+++ .github/scripts/install_genalyzer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install -y cmake tar bzip2 gzip build-essential libfftw3-dev python3-sphinx python3-breathe
+sudo apt-get install -y cmake tar bzip2 gzip build-essential libfftw3-dev python3-breathe python3-sphinx
 pip install sphinx_rtd_theme
 mkdir -p build
 cd build
