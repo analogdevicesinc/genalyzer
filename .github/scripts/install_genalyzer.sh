a7eca52400b1f18965de3c32fe5baf14f56d6208
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2021-08-25 14:54:22 -0400

    Added breathe installation to install_genalyzer.sh
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- .github/scripts/install_genalyzer.sh
+++ .github/scripts/install_genalyzer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install -y cmake tar bzip2 gzip build-essential libfftw3-dev python3-sphinx
+sudo apt-get install -y cmake tar bzip2 gzip build-essential libfftw3-dev python3-sphinx python3-breathe
 mkdir -p build
 cd build
 cmake ..
