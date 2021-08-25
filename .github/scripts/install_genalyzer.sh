b152008933befacf96ee3b36cb30dea917891a9a
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2021-08-25 15:07:52 -0400

    Added sphinx_rtd_theme installation to install_genalyzer.sh
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- .github/scripts/install_genalyzer.sh
+++ .github/scripts/install_genalyzer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y cmake tar bzip2 gzip build-essential libfftw3-dev python3-sphinx python3-breathe
+pip install sphinx_rtd_theme
 mkdir -p build
 cd build
 cmake ..
