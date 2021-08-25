be684d321c0bf8b5b672759c62fc18ce7af10a1c
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2021-08-25 13:45:15 -0400

    Added dependencies to install_genealyzer file
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- .github/scripts/install_genalyzer.sh
+++ .github/scripts/install_genalyzer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mkdir -p ../../build
-cd ../../build
+sudo apt-get install -y cmake tar bzip2 gzip build-essential fftw-dev
+mkdir -p build
+cd build
 cmake ..
 sudo make -j4
 sudo make test
