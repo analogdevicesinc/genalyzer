291dab52a91d6b94aea8f93f7d7ad598d3cc64e8
Travis F. Collins <Travis.Collins@analog.com>
2022-01-12 15:34:26 -0500

    Add graphviz to CI dependencies
    Signed-off-by: Travis F. Collins <Travis.Collins@analog.com>

--- .github/scripts/install_dependencies.sh
+++ .github/scripts/install_dependencies.sh
@@ -3,6 +3,6 @@ apt-get update
 apt-get install -y sudo
 sudo apt-get update
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-sudo apt-get install -q -y cmake tar bzip2 gzip build-essential libfftw3-dev python3-pip doxygen
+sudo apt-get install -q -y cmake tar bzip2 gzip build-essential libfftw3-dev python3-pip doxygen graphviz
 sudo pip3 install -r requirements_test.txt
 sudo pip3 install -r requirements_doc.txt
