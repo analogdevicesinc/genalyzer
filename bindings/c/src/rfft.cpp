1a2d307308617490be53987f659130583d3ea541
Travis F. Collins <travis.collins@analog.com>
2022-01-22 13:01:40 -0800

    Fix macos bug for rfft
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- bindings/c/src/rfft.cpp
+++ bindings/c/src/rfft.cpp
@@ -9,8 +9,8 @@ void gn_rfft(gn_config c, int32_t* qwf, double** fft_cplx_re, double** fft_cplx_
 {
     an::int32_vector qwvf(qwf, qwf + c->npts);
     an::cplx_vector fft_cplx(c->nfft / 2 + 1);
-    double* fft_cplx_re_tmp = (double*)calloc(c->nfft / 2, sizeof(double));
-    double* fft_cplx_im_tmp = (double*)calloc(c->nfft / 2, sizeof(double));
+    double* fft_cplx_re_tmp = (double*)calloc(c->nfft / 2 + 1, sizeof(double));
+    double* fft_cplx_im_tmp = (double*)calloc(c->nfft / 2 + 1, sizeof(double));
 
     an::rfft(qwvf.data(), qwvf.size(), fft_cplx.data(), fft_cplx.size(), c->res, c->navg, static_cast<an::diff_t>(c->nfft), c->win, c->cf);
 
