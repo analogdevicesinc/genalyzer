3d3c8422aa2bad3f3b3b5ae12acb3149e15fcf6d
Pagadarai <Srikanth.Pagadarai@analog.com>
2022-07-22 15:06:31 -0400

    Updated licenses in headers
    Signed-off-by: Pagadarai <Srikanth.Pagadarai@analog.com>

--- bindings/c/include/cgenalyzer.h
+++ bindings/c/include/cgenalyzer.h
@@ -1,7 +1,7 @@
 /*
  * cgenalyzer - genalyzer API header file
  *
- * Copyright (C) 2021 Analog Devices, Inc.
+ * Copyright (C) 2022 Analog Devices, Inc.
  * Author: Srikanth Pagadarai
  *
  * This program is free software; you can redistribute it and/or
@@ -16,9 +16,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * */
+
 
 #ifndef CGENALYZER_H
 #define CGENALYZER_H
