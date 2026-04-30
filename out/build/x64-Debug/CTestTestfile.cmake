aaa211f47b40e3b41c8cd7590b6675763171f943
Jed-Sander-Public <jed.sander@analog.com>
2026-04-30 11:49:59 -0400

    rm /out

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/git/genalyzer
-# Build directory: C:/git/genalyzer/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("bindings")
--- out/build/x64-Debug/bindings/c/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/git/genalyzer/bindings/c/src
-# Build directory: C:/git/genalyzer/out/build/x64-Debug/bindings/c/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
