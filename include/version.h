045cf9a40d018eac7702770ea3db69b22ddf318c
Travis Collins <travis.collins@analog.com>
2026-07-28 12:28:45 +0000

    ci: build native packages across platforms

--- include/version.h
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2024-2026 Analog Devices, Inc.
-//
-// SPDX short identifier: ADIBSD OR GPL-2.0-or-later
-#define GENALYZER_VERSION_MAJOR 1
-#define GENALYZER_VERSION_MINOR 0
-#define GENALYZER_VERSION_PATCH 0
